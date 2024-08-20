0bbe9124de5abbc0e58db5fc2acfda160bf4fe1b
ITACHI-MD-pixel <iu4997230@gmail.com>
2024-08-20 10:25:57 +0100

    Add files via upload

---
+++ plugins/hack.js
@@ -0,0 +1,32 @@
+const { smd, sleep } = require('../lib');
+
+smd({ cmdname: "hack", type: "fun", info: "hacking prank", filename: __filename }, async (citel) => {
+  const messages = [
+    "Injecting Malware",
+    " █ 10%",
+    " █ █ 20%",
+    " █ █ █ 30%",
+    " █ █ █ █ 40%",
+    " █ █ █ █ █ 50%",
+    " █ █ █ █ █ █ 60%",
+    " █ █ █ █ █ █ █ 70%",
+    " █ █ █ █ █ █ █ █ 80%",
+    " █ █ █ █ █ █ █ █ █ 90%",
+    " █ █ █ █ █ █ █ █ █ █ 100%",
+    "System hyjacking on process.. \\n Conecting to Server error to find 404 ",
+    "Device successfully connected... \\n Riciving data...",
+    "Data hyjacked from divice 100% completed \\n killing all evidence killing all malwares...",
+    " MALWARE 👾 SUCCESSFUL SENT TO USERS ",
+    " HACKING COMPLETED ",
+    " SENDING LOG DOCUMENTS...",
+    " SUCCESSFULLY SENT DATA AND Connection disconnected",
+    "BACKLOGS CLEARED"
+  ];
+
+  let editedMessage;
+  for (const message of messages) {
+    editedMessage = await citel.send(editedMessage || message);
+    await sleep(1000);
+    editedMessage = await citel.edit(editedMessage, message);
+  }
+});
